5c9b6f138982c17141e37d859e6896002d65ac70
abhi994918 <165630882+abhi994918@users.noreply.github.com>
2025-05-14 15:57:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+// const divEle = document.querySelector(".card-container");
+
+// function getUser(url) {
+//   return fetch(url).then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Id does not match any data");
+//     }
+//     return response.json();
+//   });
+// }
+// function getDetails(id) {
+//   getUser(`https://dummyjson.com/users/${id}`)
+//     .then((user) => {
+//       displayUser(user, "beforeend");
+//       return getUser(`https://dummyjson.com/users/${id - 1}`);
+//     })
+//     .then((user) => {
+//       displayUser(user, "afterbegin", "other");
+//       return getUser(`https://dummyjson.com/users/${id - 2}`);
+//     })
+//     .then((user) => {
+//       displayUser(user, "afterbegin", "back");
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// }
+
+// function displayUser(data, pos, className = "") {
+//   const card = `<div class="user-card ${className}">
+//         <img src=${data.image} alt="Profile Image" />
+//         <h3>${data.firstName} </h3>
+//         <h3>${data.lastName}</h3>
+//         <p class="email">${data.email}</p>
+//         <button class="btn">View Profile</button>
+//         </div>`;
+
+//   divEle.insertAdjacentHTML(pos, card);
+// }
+
+// function handleUserFetch() {
+//   const inputElement = document.getElementById("userIdInput");
+//   const userId = inputElement.valueAsNumber;
+
+//   if (!Number.isInteger(userId) || userId < 1) {
+//     alert("Please enter a valid user ID greater than 0.");
+//     return;
+//   }
+
+//   getDetails(userId);
+// }
+
+// getDetails(4);
 const divEle = document.querySelector(".card-container");
 
 function getUser(url) {
@@ -8,7 +61,10 @@ function getUser(url) {
     return response.json();
   });
 }
+
 function getDetails(id) {
+  divEle.innerHTML = ""; // Clear previous cards
+
   getUser(`https://dummyjson.com/users/${id}`)
     .then((user) => {
       displayUser(user, "beforeend");
@@ -23,19 +79,30 @@ function getDetails(id) {
     })
     .catch((err) => {
       console.error(err);
+      alert(err.message);
     });
 }
 
 function displayUser(data, pos, className = "") {
   const card = `<div class="user-card ${className}">
         <img src=${data.image} alt="Profile Image" />
-        <h3>${data.firstName} </h3>
+        <h3>${data.firstName}</h3>
         <h3>${data.lastName}</h3>
         <p class="email">${data.email}</p>
         <button class="btn">View Profile</button>
-        </div>`;
+      </div>`;
 
   divEle.insertAdjacentHTML(pos, card);
 }
 
+function handleUserFetch() {
+  const userId = parseInt(document.getElementById("userIdInput").value);
+  if (isNaN(userId) || userId < 1) {
+    alert("Please enter a valid user ID greater than 0.");
+    return;
+  }
+  getDetails(userId);
+}
+
+// Optionally preload one user
 getDetails(4);
